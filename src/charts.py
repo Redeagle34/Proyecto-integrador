e16f50c656543d6c53dc51a6423c23fac2aeaa8b
DanteRSR1507 <A01199508@tec.mx>
2025-10-02 08:50:29 -0600

    Steps Sleep relation

--- src/charts.py
+++ src/charts.py
@@ -1,1 +1,11 @@
 # Aqui se pondran las funciones de graficacion
+
+def steps_sleep_chart(data):
+    import matplotlib.pyplot as plt
+    plt.bar(results["Daily Steps ranges"], results["Quality of Sleep"])
+
+    plt.xlabel("Daily Steps")
+    plt.ylabel("Quality of Sleep")
+    plt.title("Relation between Daily Steps & Quality of Sleep")
+    plt.ylim(0,10)
+    plt.show()
--- src/statics.py
+++ src/statics.py
@@ -1,1 +1,12 @@
 # Aqui poner las funciones de estadisticas
+import pandas as pd
+
+def steps_sleep(data):
+    bins = [2000,4000,6000,8000,10000]
+    labels = ["2000-4000", "4000-6000", "6000-8000", "8000-10000"]
+
+    data["Daily Steps ranges"] = pd.cut(data["Daily Steps"], bins=bins, labels=labels)
+
+    results = data.groupby("Daily Steps ranges")["Quality of Sleep"].mean().reset_index()
+
+    print(results)
